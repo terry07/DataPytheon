e0857ac3c3067d512fd2d6ad0fe51ba83bab9650
Stamatis Karlos <stkarlos@csd.auth.gr>
2025-07-12 12:56:32 +0300

    feat: titanic enhancements (#3)
    - Detect with pythonic way the most common value for imputation,
    - Insert a threshold-base criterion for dropping whole column instead of removing missing rows (it is designer's choice based on the context of the problem),
    - Add some minimal info regarding data structure and the main actions that took place during preprocessing (not the ad-hoc ones).

--- src/recipes/titanic.py
+++ src/recipes/titanic.py
@@ -19,19 +19,38 @@ def load_titanic_data():
     # Load dataset from seaborn
     df = sns.load_dataset("titanic")
 
+    # Display initial structure the dataset
+    print("==Initial dataset structure==")
+    print(df.shape, "\n")
+
     # Drop columns with too many missing values or redundant info
     df = df.drop(columns=["deck", "embark_town", "alive"])
 
-    # Drop rows with critical missing values
-    df = df.dropna(subset=["embarked", "age"])
+    # Drop columns with more than p(%) missing values - adjust p as needed
+    # Here, we set p to 10% as an example
+    p = 0.10 
+    threshold = p * len(df)
+    cols_to_drop = [col for col in df.columns if df[col].isnull().sum() > threshold]
+    print(f"Dropping columns with more than 10% missing values: {cols_to_drop} \n")
+    df = df.drop(columns=cols_to_drop)
 
     # Fill missing values in 'embarked' with the most common port
-    df["embarked"] = df["embarked"].fillna("S")
+
+    # First, detect the most common value (mode) in the 'embarked' column
+    most_common_embarked = df["embarked"].mode()[0]
+    print(f"Most common embarked value: {most_common_embarked} \n")
+
+    # Then, fill missing values with the most common value
+    df["embarked"] = df["embarked"].fillna(most_common_embarked)
 
     # Convert categorical columns to category dtype
     cat_cols = ["sex", "class", "embarked", "who", "adult_male", "alone"]
     for col in cat_cols:
         df[col] = df[col].astype("category")
+
+    # Display structure of the dataset after our prepriocessing
+    print("==Final dataset structure==")
+    print(df.shape, "\n")
 
     return df
 
